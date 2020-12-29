9a11e3f4ec426afee34b636994b1da41a0f47523
nngarfdn <33192900+nngarfdn@users.noreply.github.com>
2020-12-29 07:27:02 +0000

    login with google, wilayah, upload gambar

--- app/src/main/java/com/example/ternavest/MainActivity.java
+++ app/src/main/java/com/example/ternavest/MainActivity.java
@@ -1,21 +1,51 @@
 package com.example.ternavest;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.ternavest.testing.WilayahTest;
 import com.example.ternavest.ui.peternak.PeternakActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private final String TAG = getClass().getSimpleName();
+    private static final int RC_SIGN_IN = 9001;
+
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient googleSignInClient;
+
+    private EditText edtEmail, edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
 
         Button btnTestWilayah = findViewById(R.id.btn_testwilayah);
         btnTestWilayah.setOnClickListener(v -> startActivity(new Intent(this, WilayahTest.class)));
@@ -23,5 +53,63 @@ public class MainActivity extends AppCompatActivity {
         Button btnPeternak = findViewById(R.id.btn_peternak);
         btnPeternak.setOnClickListener(v -> startActivity(new Intent(this, PeternakActivity.class)));
 
+        Button btnGoogle = findViewById(R.id.btn_google_login);
+        btnGoogle.setOnClickListener(v -> loginWithGoogle() );
     }
+
+    private void loginWithGoogle(){
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e){
+                // Google Sign In failed or user press back button
+                Log.w(TAG, "Google sign in failed", e);
+                //showSnackbar(getActivity().findViewById(R.id.activity_login), "Email gagal diautentikasi.");
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account){
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+//        loadingDialog.show();
+
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        firebaseAuth.signInWithCredential(authCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            // Sign in success, update UI with the signed-in user's informationZ
+                            Log.d(TAG, "signInWithCredential: success");
+                            launchMain();
+                        } else {
+                            // If sign in fails, display a message to the user
+                            Log.w(TAG, "signInWithCredential: failure", task.getException());
+//                            showSnackbar(this.findViewById(R.id.activity_login), "Email gagal diautentikasi.");
+
+                        }
+
+//                        loadingDialog.dismiss();
+                    }
+                });
+    }
+
+    private void launchMain(){
+        Intent intent = new Intent(MainActivity.this, PeternakActivity.class);
+        startActivity(intent);
+        this.finish();
+    }
+
 }
--- app/src/main/java/com/example/ternavest/ui/peternak/kelola/TambahProyekActivity.kt
+++ app/src/main/java/com/example/ternavest/ui/peternak/kelola/TambahProyekActivity.kt
@@ -2,38 +2,62 @@ package com.example.ternavest.ui.peternak.kelola
 
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.InputType
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.webkit.MimeTypeMap
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.CompoundButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.NewInstanceFactory
 import com.example.ternavest.R
 import com.example.ternavest.model.Location
 import com.example.ternavest.viewmodel.LocationViewModel
 import com.example.ternavest.viewmodel.ProyekViewModel
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_tambah_proyek.*
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 
-class TambahProyekActivity : AppCompatActivity() {
+@Suppress("UNREACHABLE_CODE")
+class TambahProyekActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
 
     private val TAG = javaClass.simpleName
 
-
     private var fromDatePickerDialog: DatePickerDialog? = null
     private var toDatePickerDialog: DatePickerDialog? = null
-
+    private val PICK_IMAGE_REQUEST = 22
     private var dateFormatter: SimpleDateFormat? = null
+    var objectStorageReference: StorageReference? = null
+    var objectFirebaseFirestore: FirebaseFirestore? = null
 
     private lateinit var proyekViewModel: ProyekViewModel
     private lateinit var lvm: LocationViewModel
-    private lateinit var listProvinces: ArrayList<Location>
-    private lateinit var listRegencies: ArrayList<Location>
-    private lateinit var listDistricts : ArrayList<Location>
+    private  var listProvinces: ArrayList<Location> = ArrayList()
+    private  var listRegencies: ArrayList<Location> = ArrayList()
+    private  var listDistricts : ArrayList<Location> = ArrayList()
     private var idProvince = 0
     private var idRegency = 0
     private var idDistrict = 0
+    private var filePath: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +68,9 @@ class TambahProyekActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         proyekViewModel = ViewModelProvider(this, NewInstanceFactory()).get(ProyekViewModel::class.java)
+        lvm = ViewModelProvider(this, NewInstanceFactory()).get(LocationViewModel::class.java)
+        objectStorageReference = FirebaseStorage.getInstance().getReference("imageFolder")
+        objectFirebaseFirestore = FirebaseFirestore.getInstance()
 
         val namaProyek = txtNamaProyek.text.toString()
         val deskripsiProyek = txtDeskripsiProyek.text.toString()
@@ -56,11 +83,41 @@ class TambahProyekActivity : AppCompatActivity() {
         dateFormatter = SimpleDateFormat("dd-MM-yyyy", Locale.US)
         txtWaktuMulai.setInputType(InputType.TYPE_NULL)
         txtWaktuMulai.requestFocus()
-
         txtWaktuSelesai.setInputType(InputType.TYPE_NULL)
         setDateTimeField()
+        initWilayah()
+        loadProvinces()
 
+        btnUploadImage.setOnClickListener { selectImage() }
 
+    }
+
+    private fun initWilayah() {
+        cb_provinces.setEnabled(true)
+        cb_regencies.setEnabled(false)
+        cb_districts.setEnabled(false)
+        spin_provinces.setEnabled(false)
+        spin_regencies.setEnabled(false)
+        spin_districts.setEnabled(false)
+
+        cb_provinces.setOnCheckedChangeListener(this)
+        cb_regencies.setOnCheckedChangeListener(this)
+        cb_districts.setOnCheckedChangeListener(this)
+        spin_provinces.setOnItemSelectedListener(this)
+        spin_regencies.setOnItemSelectedListener(this)
+        spin_districts.setOnItemSelectedListener(this)
+        cb_provinces.isEnabled = true
+        cb_regencies.isEnabled = false
+        cb_districts.isEnabled = false
+        spin_provinces.setEnabled(false)
+        spin_regencies.setEnabled(false)
+        spin_districts.setEnabled(false)
+        cb_provinces.setOnCheckedChangeListener(this)
+        cb_regencies.setOnCheckedChangeListener(this)
+        cb_districts.setOnCheckedChangeListener(this)
+        spin_provinces.setOnItemSelectedListener(this)
+        spin_regencies.setOnItemSelectedListener(this)
+        spin_districts.setOnItemSelectedListener(this)
     }
 
     private fun setDateTimeField() {
@@ -86,4 +143,233 @@ class TambahProyekActivity : AppCompatActivity() {
         onBackPressed()
         return true
     }
+
+    private fun loadProvinces() {
+        lvm.loadProvinces()
+        lvm.provinces.observe(this, Observer { provinces ->
+            if (provinces != null) {
+                listProvinces = java.util.ArrayList()
+                val itemList: MutableList<String> = java.util.ArrayList()
+                for (attributes in provinces.provinces) { // Fix nama provinsi
+                    if (attributes.id == 31) listProvinces.add(Location(attributes.id, "DKI Jakarta")) else if (attributes.id == 34) listProvinces.add(Location(attributes.id, "DI Yogyakarta")) else listProvinces.add(Location(attributes.id, attributes.name))
+                }
+                for (location in listProvinces) itemList.add(location.name)
+                val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, itemList)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spin_provinces.setAdapter(adapter)
+            }
+        })
+    }
+
+    private fun loadRegencies(idProvince: Int) {
+        lvm.loadRegencies(idProvince)
+        lvm.regencies.observe(this, Observer { regencies ->
+            if (regencies != null) {
+                listRegencies = java.util.ArrayList()
+                val itemList: MutableList<String> = java.util.ArrayList()
+                for (attributes in regencies.regencies) listRegencies.add(Location(attributes.id, attributes.name))
+                for (location in listRegencies) itemList.add(location.name)
+                val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, itemList)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spin_regencies.setAdapter(adapter)
+            }
+        })
+    }
+
+    private fun loadDistricts(idRegency: Int) {
+        lvm.loadDistricts(idRegency)
+        lvm.districts.observe(this, Observer { districts ->
+            if (districts != null) {
+                listDistricts = java.util.ArrayList()
+                val itemList: MutableList<String> = java.util.ArrayList()
+                for (attributes in districts.districts) listDistricts.add(Location(attributes.id, attributes.name))
+                for (location in listDistricts) itemList.add(location.name)
+                val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, itemList)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spin_districts.setAdapter(adapter)
+            }
+        })
+    }
+
+    override fun onItemSelected(adapterView: AdapterView<*>, view: View?, i: Int, l: Long) {
+        when (adapterView.id) {
+            R.id.spin_provinces -> {
+                idProvince = listProvinces[i].id
+                loadRegencies(idProvince)
+            }
+            R.id.spin_regencies -> {
+                idRegency = listRegencies[i].id
+                loadDistricts(idRegency)
+            }
+            R.id.spin_districts -> idDistrict = listDistricts[i].id
+        }
+    }
+
+    override fun onNothingSelected(adapterView: AdapterView<*>?) {}
+
+    override fun onCheckedChanged(compoundButton: CompoundButton, b: Boolean) {
+        when (compoundButton.id) {
+            R.id.cb_provinces -> {
+                spin_provinces.setEnabled(b)
+                cb_regencies.isEnabled = b
+                if (!b) {
+                    cb_regencies.isChecked = false
+                    cb_districts.isChecked = false
+                }
+            }
+            R.id.cb_regencies -> {
+                spin_regencies.isEnabled = b
+                cb_districts.isEnabled = b
+                if (!b) cb_districts.isChecked = false
+            }
+            R.id.cb_districts -> spin_districts.isEnabled = b
+        }
+    }
+
+    private fun selectImage() {
+        // Defining Implicit Intent to mobile gallery
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                PICK_IMAGE_REQUEST)
+    }
+
+    private fun getExtension(uri: Uri): String? {
+        try {
+            val objectContentResolver: ContentResolver = Objects.requireNonNull(this).getContentResolver()
+            val objectMimeTypeMap = MimeTypeMap.getSingleton()
+            return objectMimeTypeMap.getExtensionFromMimeType(objectContentResolver.getType(uri))
+        } catch (e: Exception) {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+        return null
+    }
+
+    private fun uploadImage() {
+        if (filePath != null) {
+//            txt_uploading.setVisibility(View.VISIBLE)
+            btnUploadImage.setText("Mengupload..")
+            val namaImage = UUID.randomUUID().toString() // diganti id produk di firebase
+            val nameOfimage = namaImage + "." + getExtension(filePath!!)
+            val imageRef = objectStorageReference!!.child(nameOfimage)
+            val objectUploadTask = imageRef.putFile(filePath!!)
+            objectUploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot?, Task<Uri>> { task: Task<UploadTask.TaskSnapshot?> ->
+                if (!task.isSuccessful) {
+                    throw Objects.requireNonNull(task.exception)!!
+                }
+                imageRef.downloadUrl
+            }).addOnCompleteListener { task: Task<Uri?> ->
+                if (task.isSuccessful) {
+//                    txt_uploading.setVisibility(View.INVISIBLE)
+                    btnUploadImage.visibility = View.INVISIBLE
+                    Toast.makeText(this, "Upload Gambar Berhasil", Toast.LENGTH_SHORT).show()
+//                    btn_tambah_produk.setOnClickListener(View.OnClickListener { v: View? ->
+//                        val photo = Objects.requireNonNull(task.result).toString()
+//                        Log.d(TAG, "uploadImage: photoUrl : $photo")
+//                        val email = firebaseUser!!.email
+//                        val nama: String = edt_nama_produk.getText().toString()
+//                        val kategori: String = spinner_kategori.getSelectedItem().toString()
+//                        val alamat: String = edt_alamat_produk.getText().toString()
+//                        var kecamatan: String? = null
+//                        kecamatan = if (spin_districts != null && spin_districts.getSelectedItem() != null) {
+//                            spin_districts.getSelectedItem() as String
+//                        } else {
+//                            "-"
+//                        }
+//                        var kabupaten: String? = null
+//                        kabupaten = if (spin_regencies != null && spin_regencies.getSelectedItem() != null) {
+//                            spin_regencies.getSelectedItem() as String
+//                        } else {
+//                            "-"
+//                        }
+//
+//                        var prov: String? = null
+//                        prov = if (spin_provinces != null && spin_provinces.getSelectedItem() != null) {
+//                            spin_provinces.getSelectedItem() as String
+//                        } else {
+//                            "-"
+//                        }
+//
+//                        val wa: String = edt_alamat_nowa.getText().toString()
+//                        val harga: String = edt_alamat_harga.getText().toString()
+//                        val hargaInt = harga.toInt()
+//                        val deskripsi: String = edt_alamat_deskripsi.getText().toString()
+//
+//                        var cek = true
+//                        if (nama.length <= 0) {
+//                            edt_nama_produk.setError("Masukkan nama produk/barang/jasa")
+//                            cek = false
+//                        }
+//
+//                        if (TextUtils.isEmpty(alamat)) {
+//                            edt_alamat_produk.setError("Masukkan alamat produk/barang/jasa")
+//                            cek = false
+//                        }
+//
+//                        if (TextUtils.isEmpty(wa)) {
+//                            edt_alamat_nowa.setError("Masukkan nomor WhatsApp")
+//                            cek = false
+//                        }
+//
+//                        if (!isValidPhone(wa)) {
+//                            edt_alamat_nowa.setError("Awali nomor dengan 628xxx")
+//                            cek = false
+//                        }
+//
+//                        if (TextUtils.isEmpty(harga)) {
+//                            edt_alamat_harga.setError("Masukkan harga")
+//                            cek = false
+//                        }
+//
+//                        if (TextUtils.isEmpty(deskripsi)) {
+//                            edt_alamat_deskripsi.setError("Masukkan deskripsi")
+//                            cek = false
+//                        }
+//
+//                        if (TextUtils.isEmpty(photo)) {
+//                            Toast.makeText(this, "Upload foto dulu", Toast.LENGTH_SHORT).show()
+//                            cek = false
+//                        }
+//
+//                        val p = Produk(produk.id, email!!, nama, kategori, alamat, kecamatan,
+//                                kabupaten, prov, wa, hargaInt, deskripsi, photo)
+//
+//                        if (cek) {
+//                            produkViewModel!!.updateProduk(p)
+//                            Toast.makeText(this, "Update Berhasil", Toast.LENGTH_SHORT).show()
+//                            finish()
+//                        }
+//                    })
+                } else if (!task.isSuccessful) {
+                    Toast.makeText(this, Objects.requireNonNull(task.exception).toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+            // Get the Uri of data
+            filePath = data.data
+            try {
+                // Setting image on image view using Bitmap
+                val bitmap = MediaStore.Images.Media
+                        .getBitmap(
+                                Objects.requireNonNull(this).getContentResolver(),
+                                filePath)
+                imgUpload.setImageBitmap(bitmap)
+                btnUploadImage.setText("Upload")
+                btnUploadImage.setOnClickListener{ uploadImage() }
+            } catch (e: IOException) {
+                // Log the exception
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
