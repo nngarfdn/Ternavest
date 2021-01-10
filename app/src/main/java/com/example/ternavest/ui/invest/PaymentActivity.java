97101586edf9652252e6e127f1327966845edb17
ariefzuhri <ariefzuhri72@gmail.com>
2021-01-10 13:33:47 +0700

    Buat layout pembayaran

---
+++ app/src/main/java/com/example/ternavest/ui/invest/PaymentActivity.java
@@ -0,0 +1,16 @@
+package com.example.ternavest.ui.invest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ternavest.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
